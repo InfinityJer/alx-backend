3478f283e5468265894f854899d298a9017d8b97
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2024-01-29 16:12:46 +0800

    Update 1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -13,7 +13,7 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
         page_size (int): Number of items per page.
 
     Returns:
-        Tuple[int, int]: Start and end indexes for the specified pagination parameters.
+        Tuple[int, int]: Start/end indexes for specified pagination parameters.
     """
     return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
 
