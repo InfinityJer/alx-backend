1b2e0b28121bb651296e74c97ba8bbb960a84a59
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2024-01-31 16:15:18 +0800

    Update 100-lfu_cache.py

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -21,9 +21,10 @@ class LFUCache(BaseCaching):
                 # Find the least frequency used item
                 min_frequency = min(self.frequency_counter.values())
                 candidates = [k for k, v in self.frequency_counter.items()
-                        if v == min_frequency]
-                # Use LRU algorithm to discard only the lru if more than one 
-                discarded_key = min(candidates, key=lambda k: self.cache_data[k])
+                              if v == min_frequency]
+                # Use LRU algorithm to discard only the lru if more than one
+                discarded_key = min(candidates, key=lambda k:
+                                    self.cache_data[k])
                 del self.cache_data[discarded_key]
                 del self.frequency_counter[discarded_key]
                 print("DISCARD: {}".format(discarded_key))
